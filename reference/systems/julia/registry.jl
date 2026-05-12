798d0adf9360b5dfe3a09acf4c00a7329b5dd3b8
Lawrence Berry <lawrenceberry@outlook.com>
2026-05-12 17:18:48 +0100

    Remove unneeded ODE system test cases

--- reference/systems/julia/registry.jl
+++ reference/systems/julia/registry.jl
@@ -1,42 +1,24 @@
 include("coupled_vdp_lattice.jl")
-include("damped_rotation.jl")
 include("heat_equation.jl")
 include("bateman.jl")
-include("nn_reactions.jl")
-include("reversible_trapping.jl")
-include("moving_diffusion_spike.jl")
 include("lorenz.jl")
 include("robertson.jl")
-include("vdp.jl")
 include("kaps.jl")
-include("stiff_scalar.jl")
 include("brusselator.jl")
 
 function make_system_spec(system_name::String, config)
     if system_name == "coupled_vdp_lattice"
         return make_coupled_vdp_lattice_spec(config)
-    elseif system_name == "damped_rotation"
-        return make_damped_rotation_spec(config)
     elseif system_name == "heat_equation"
         return make_heat_equation_spec(config)
     elseif system_name == "bateman"
         return make_bateman_spec(config)
-    elseif system_name == "nn_reactions"
-        return make_nn_reactions_spec(config)
-    elseif system_name == "reversible_trapping"
-        return make_reversible_trapping_spec(config)
-    elseif system_name == "moving_diffusion_spike"
-        return make_moving_diffusion_spike_spec(config)
     elseif system_name == "lorenz"
         return make_lorenz_spec(config)
     elseif system_name == "robertson"
         return make_robertson_spec(config)
-    elseif system_name == "vdp"
-        return make_vdp_spec(config)
     elseif system_name == "kaps"
         return make_kaps_spec(config)
-    elseif system_name == "stiff_scalar"
-        return make_stiff_scalar_spec(config)
     elseif system_name == "brusselator"
         return make_brusselator_spec(config)
     end
